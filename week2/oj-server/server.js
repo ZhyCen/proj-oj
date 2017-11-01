9a6a356af1afc1e03528acbf5c3bee495ca4ef2e
zhycen <cenzheyang@gmail.com>
2017-11-01 13:30:30 -0700

    backend stage 1

---
+++ week2/oj-server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+var restRouter = require('./routes/rest')
+
+
+app.use("/api/v1", restRouter);
+
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!')
+})
