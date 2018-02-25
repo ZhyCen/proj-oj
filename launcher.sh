34322a933049853f93b3d9f0c0cd74f158140769
zhycen <cenzheyang@gmail.com>
2018-02-25 13:04:00 -0800

    make tmp folder

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,7 @@
 fuser -k 3000/tcp
 fuser -k 5000/tcp
 
-service redis_6379 start
+sudo service redis_6379 start
 cd ./oj-server
 npm install
 nodemon server.js &
@@ -10,12 +10,14 @@ cd ../oj-client
 npm install
 ng build --watch &
 cd ../executor
+mkdir tmp
 pip install -r requirements.txt
-python executor_server.py &
+sudo python executor_server.py &
 
 echo "=================================================="
 read -p "PRESS [ENTER] TO TERMINATE PROCESSES." PRESSKEY
 
+sudo rm -rf tmp
 fuser -k 3000/tcp
 fuser -k 5000/tcp
 service redis_6379 stop
